7050c10fe0290338fa2f688c8512be7ce561372f
Jon Dahl <jon@dira.no>
2021-08-25 14:43:25 +0200

    Add flipped dictionary of element names and corresponding proton number

--- kshell_utilities/kshell_utilities.py
+++ kshell_utilities/kshell_utilities.py
@@ -1,5 +1,4 @@
-import os
-import sys
+import os, sys
 from fractions import Fraction
 import numpy as np
 
@@ -7,6 +6,12 @@ atomic_numbers = {
     "oxygen": 8, "fluorine": 9, "neon": 10, "sodium": 11, "magnesium": 12,
     "aluminium": 13, "silicon": 14, "phosphorus": 15, "sulfur": 16,
     "chlorine": 17, "argon": 18
+}
+# atomic_numbers_reversed = dict([(y, x) for x,y in ksutil.atomic_numbers.items()])
+atomic_numbers_reversed = {
+    8: 'oxygen', 9: 'fluorine', 10: 'neon', 11: 'sodium', 12: 'magnesium',
+    13: 'aluminium', 14: 'silicon', 15: 'phosphorus', 16: 'sulfur',
+    17: 'chlorine', 18: 'argon'
 }
 
 def create_jpi_list(spins, parities):
