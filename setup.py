837d3761e7273151b0baf5ede91894cbe29581a6
Jon Dahl <jon@dira.no>
2021-09-29 19:12:27 +0200

    Update setup parameters

--- setup.py
+++ setup.py
@@ -2,19 +2,18 @@ from setuptools import setup
 
 setup(
     name = 'kshell_utilities',
-    version = '0.1.0',    
+    version = '0.2.0',    
     description = 'Handy utilities for handling nuclear shell model calculations from KSHELL',
-    url = 'https://github.com/JonKDahl/kshell_utilities',
+    url = 'https://github.com/GaffaSnobb/kshell_utilities',
     author = 'Jon Kristian Dahl',
     author_email = 'jonkd@uio.no',
-    license = '“Commons Clause” License Condition v1.0',
     packages = ['kshell_utilities', 'tests'],
     install_requires = ['numpy', 'matplotlib'],
 
     classifiers = [
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
-        'Operating System :: MacOS',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering :: Physics',
     ],
