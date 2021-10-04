f85d1f4b93e15ccd6daa0a9daa87f11745ac8b2c
Jon Dahl <jon@dira.no>
2021-10-04 16:32:07 +0200

    Add count_dim file, add version legend

--- kshell_utilities/__init__.py
+++ kshell_utilities/__init__.py
@@ -2,7 +2,18 @@ __version__ = "1.0.1.3"
 __author__ = "Jon Kristian Dahl"
 __credits__ = "Noritaka Shimizu, Jørgen Eriksson Midtbø"
 
+"""
+Version legend:
+a.b.c.d
+
+a: major release
+b: new functionality added
+c: new feature to existing functionality
+d: bug fixes
+"""
+
 from .kshell_utilities import *
 from .general_utilities import *
 from .kshell_exceptions import *
+from .count_dim import *
 # from .low_energy_enhancement import * # Not ready for ver. 1.0
