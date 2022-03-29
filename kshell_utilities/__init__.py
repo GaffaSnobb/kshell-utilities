8d48998af0de8ad7e6e01685a932df4b925d3a40
Jon Dahl <jon@dira.no>
2022-03-29 12:32:07 +0200

    Import new script editing functionality

--- kshell_utilities/__init__.py
+++ kshell_utilities/__init__.py
@@ -18,4 +18,5 @@ from .kshell_exceptions import *
 from .count_dim import *
 from .parameters import *
 from .collect_logs import collect_logs, check_multipolarities
+from .script_editing import edit_executables
 # from .low_energy_enhancement import * # Not ready for ver. 1.0
