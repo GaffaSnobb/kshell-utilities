bfbf70270f1e868237aaad695d2e4860b662dcd0
Jon Dahl <jon@dira.no>
2022-04-27 17:00:05 +0200

    Include function for changing default plt.rcParams values

--- kshell_utilities/parameters.py
+++ kshell_utilities/parameters.py
@@ -8,6 +8,20 @@ def debug_mode(switch):
     else:
         print(f"Invalid debug switch '{switch}'")
 
+def latex_plot():
+    import matplotlib.pyplot as plt
+    plt.rcParams.update({
+        "backend": "pgf",
+        "text.usetex": True,
+        "font.family": "serif",
+        "font.serif": ["roman"],
+        "legend.fontsize": 14,
+        "xtick.labelsize": 15,
+        "ytick.labelsize": 15,
+        "axes.labelsize": 14,
+        "axes.titlesize": 15,
+    })
+
 atomic_numbers = {
     "oxygen": 8, "fluorine": 9, "neon": 10, "sodium": 11, "magnesium": 12,
     "aluminium": 13, "silicon": 14, "phosphorus": 15, "sulfur": 16,
