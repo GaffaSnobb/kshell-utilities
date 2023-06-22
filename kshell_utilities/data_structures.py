423286066e8b89c6e16f5d401af498498737799f
Jon Dahl <jon@dira.no>
2023-06-22 12:00:17 +0900

    Add docstrings and ho_quanta attributes

--- kshell_utilities/data_structures.py
+++ kshell_utilities/data_structures.py
@@ -16,15 +16,16 @@ class OrbitalParameters:
     """
     For storing parameters of the model space orbitals.
     """
-    idx: int        # Index of the orbital from the partition file.
-    n: int
+    idx: int        # Index of the orbital from the interaction file.
+    n: int          # The "principal quantum number".
     l: int          # Orbital angular momentum.
     j: int          # Total angular momentum.
     tz: int         # Isospin.
-    nucleon: str    # Proton or neutron
-    name: str       # Ex: p 0d5/2.
+    nucleon: str    # 'p' or 'n'.
+    name: str       # Ex: 'p 0d5/2'.
     parity: int
     order: OrbitalOrder
+    ho_quanta: int  # Harmonic oscillator quanta (2*n + l) of the orbital (little bit unsure if this is a good name). Used to limit the possible combinations of pn configuration combinations.
 
     def __str__(self):
         return self.name
@@ -48,9 +49,37 @@ class Interaction:
     n_core_protons: int
     n_core_neutrons: int
 
-
 @dataclass
 class ConfigurationParameters:
+    """
+    Terminology:
+        - "Occupation" refers to the number of particles occupying one
+        orbital. Ex: 1 is an occupation.
+        
+        - "Configuration" refers to a set of orbitals with a given
+        occupation. Ex: [0, 1, 1, 0, 0, 0] is a configuration.
+
+        - "Partition" refers to a set of configurations with a given
+        number of particles. Ex: [[0, 1, 1, 0, 0, 0], [0, 1, 0, 1, 0, 0]]
+        is a partition.
+
+    Parameters
+    ----------
+    idx : int
+        Index of the configuration.
+    
+    parity : int
+        The parity of the configuration.
+
+    configuration : list[int]
+        The configuration as a list of occupations. Ex: [0, 1, 1, 0, 0, 0].
+
+    ho_quanta : int
+        The sum of the harmonic oscillator quanta of all the particles
+        in the configuration. Used to limit the possible combinations of
+        pn configuration combinations when using hw truncation.
+    """
     idx: int
     parity: int
     configuration: list[int]
+    ho_quanta: int
