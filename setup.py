58676980e9479dc9cb675e8f2f2a951d967e67e6
Jon Dahl <jon@dira.no>
2023-11-16 20:21:08 +0900

    Add package data

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
 from setuptools import setup
 
-# try:
-#     import pypandoc
-#     long_description = pypandoc.convert('README.md', 'rst')
-# except (IOError, ImportError, OSError):
-#     long_description = ""
-
 setup(
     name = 'kshell-utilities',
     version = '1.5.1.0',    
     description = 'Handy utilities for handling nuclear shell model calculations from KSHELL',
-    # long_description = long_description,
     url = 'https://github.com/GaffaSnobb/kshell-utilities',
     author = ['Jon Kristian Dahl', 'Johannes Heines'],
     author_email = 'jonkd@uio.no',
     packages = ['kshell_utilities'],
     install_requires = ['numpy', 'matplotlib', 'seaborn', 'scipy', 'numba', 'vum'],
+    package_data = {
+        'kshell_utilities': ['test_files/*.txt'],
+    },
 
     classifiers = [
         'Development Status :: 5 - Production/Stable',
