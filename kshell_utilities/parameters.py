ba9a2b7e5098e6085938990193c88578b90d291b
Jon Dahl <jon@dira.no>
2025-03-10 09:18:31 +0100

    Add mpl save format

--- kshell_utilities/parameters.py
+++ kshell_utilities/parameters.py
@@ -3,7 +3,8 @@ from .data_structures import OrbitalOrder
 
 GS_FREE_PROTON = 5.585
 GS_FREE_NEUTRON = -3.826
-DPI = 600   # DPI for plots.
+DPI = 400   # DPI for plots.
+MATPLOTLIB_SAVEFIG_FORMAT = "png"
 FIGSIZE = (7, 6.4)  # Figsize for plots.
 flags = {"debug": False, "parallel": True}
 
